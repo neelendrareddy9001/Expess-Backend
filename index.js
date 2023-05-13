1854963090d32ff521c5bd8873802f45a9c75434
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-13 16:46:45 +0200

    Styling & folder

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
