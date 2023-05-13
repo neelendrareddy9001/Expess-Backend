786b68b4bbcb20d1f7915c5b7739fe10c67f2ed0
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-13 23:05:55 +0200

    Added

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/Logger');
 
 const app = express();
+const hbs = exphbs.create({/* config */})
 
 //Init middleware
 //app.use(logger);
+
 
 //Body Parseer Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// })
-
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Homepate Routes
+app.get('/', (req,res) => res.render('index', {
+    title: 'Member App'
+}));
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 
 //Member API Routes
 app.use('/api/members', require('./routes//api/members'));
