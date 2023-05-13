7fbbd1e61498d9f118766bf62863f50d985c6efa
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-13 16:39:41 +0200

    added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
