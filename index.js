58b3ace8872a9ef0d8e12eea782c168ebb470fc4
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-14 23:37:46 +0200

    Added

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const logger = require('./middleware/Logger');
-
-
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
-// const hbs = exphbs.create({/* config */})
 
-//Init middleware
-//app.use(logger);
+// Init middleware
+// app.use(logger);
 
-// //Handlebars Middleware
-app.engine('handlebars', exphbs());
+// Handlebars Middleware
+app.engine('handlebars', exphbs.engine({ extname: 'handlebars', defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-    res.render('index');
-})
-
-//Homepage Routes
-app.set('view engine', 'handlebars');
-
-//Body Parseer Middleware
+// Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-//Set static folder
-app.use(express.static(path.join(__dirname, './public')));
+// Homepage Route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members
+  })
+);
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Member API Routes
-app.use('/api/members', require('./routes//api/members'));
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
